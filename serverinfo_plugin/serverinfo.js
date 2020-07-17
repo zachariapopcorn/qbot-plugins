78fa28b81ca2d0648cf87e639165f39a11a55bc3
MyNameIsAutistic <65930787+MyNameIsAutistic@users.noreply.github.com>
2020-07-17 13:13:18 -0700

    Update serverinfo.js

--- serverinfo_plugin/serverinfo.js
+++ serverinfo_plugin/serverinfo.js
@@ -4,10 +4,10 @@ const server = message.guild
 const e = new MessageEmbed()
 .setColor('RANDOM')
 .setTitle(`Info on guild ${server.name}.`)
-.addField('Server ID',[`${server.id}`],true)
-.addField('Members',[`${server.memberCount}`],true)
-.addField('Roles',[`${server.roles.cache.size}`],true)
-.addField('Owner',[`<@${server.owner.id}>`],true)
+.addField('Server ID',`${server.id}`,true)
+.addField('Members',`${server.memberCount}`,true)
+.addField('Roles',`${server.roles.cache.size}`,true)
+.addField('Owner',`<@${server.owner.id}>`,true)
 message.channel.send(e)
 }
 
