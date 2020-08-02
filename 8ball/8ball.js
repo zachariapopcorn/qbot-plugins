4101eba7b594d58000c97bd2e40c121da3d3cac8
bigbenster702 <48159706+bigbenster702@users.noreply.github.com>
2020-08-02 00:56:25 -0500

    Add files via upload

---
+++ 8ball/8ball.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+
+exports.run = async (bot, message, args) => {
+
+if (!args[0]) return message.reply("Please ask a question!");
+	
+	let replies = ["Yes.", "No.", "Ask again later.", "Maybe.", "Yes and definitely.", "It is certain.", "As I see it, yes.", "Very doubtful.", "Eh I will say yes to that.", "NO!", "Never.", "Nope."];
+	
+	let result = Math.floor((Math.random() * replies.length));
+	let question = args.slice(0).join(" ");
+
+	let ballembed = new Discord.MessageEmbed()
+	.setAuthor(message.author.tag)
+	.setColor("#FF9900")
+	.addField("Question", question)
+	.addField("Answer", replies[result])
+	.setFooter(`qbot plugin by bigbenster702`);
+
+message.channel.send(ballembed)
+}
---
+++ 8ball/README.md
@@ -0,0 +1,12 @@
+# qbot 8ball plugin
+This plugin will add a magic 8 ball command.
+
+## Instructions
+* Make a file named `8ball.js` inside of the `commands` folder.
+* Copy and paste code from `8ball.js` found on this repository into your editor/host.
+
+## If you want to chnage the answers read this.
+* To change/add answers go to `8ball.js` then go to where it says `let replies = ` and in the [] you can change the answers.
+* Template of adding them `["text here", "text here2", "text here"]` and etc.
+
+* I am gessing you know this but where it says text here that is where you put your answers.
