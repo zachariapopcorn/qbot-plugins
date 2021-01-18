eed487f426fc5335753ae0ed0a2ef0a85ce77904
zachariapopcorn <52849920+zachariapopcorn@users.noreply.github.com>
2021-01-17 20:02:25 -0800

    Update eval.js

--- eval_plugin/eval.js
+++ eval_plugin/eval.js
@@ -1,32 +1,44 @@
-exports.run = (client, message, args) => {
-    if(message.author.id != "YOUR USER ID HERE") return;
-    if(!args[0]) {
-        return message.channel.send("Please provide code for me to run!");
-    }
-    try {
-        const code = args.join(" ");
-        let evaled = eval(code);
-        if(typeof evaled != "string") {
-            evaled = require("util").inspect(evaled);
+const Discord = require('discord.js');
+
+const config = {
+    description: 'Executes code on the Node.js process',
+    aliases: ['execute'],
+    usage: '<code>',
+    rolesRequired: ['Execution Permissions'],
+    category: 'Execution'
+}
+
+module.exports = {
+    config,
+
+    /**
+    * @param {Discord.Message} message
+    * @param {Discord.Client} client
+    * @param {String[]} args
+    */
+
+    run: async (client, message, args) => {
+        
+        function embedMaker(type, title, description) {
+            let embed = new Discord.MessageEmbed();
+            embed.setColor(client.config.colors[type]);
+            embed.setTitle(title);
+            embed.setDescription(description);
+            embed.setFooter("Plugin made by zachariapopcorn#8105");
+            return embed;
         }
-    } catch (err) {
-        return message.channel.send({embed: {
-            color: 7948427,
-            description: `**Error:**\n`
-            + `\`There was an error while compiling your code: ${err}\``,
-            author: {
-                name: message.author.tag,
-                icon_url: message.author.displayAvatarURL()
-            }
-        }});
-    }
-    return message.channel.send({embed: {
-        color: 7948427,
-        description: `**Success:**\n`
-            + `\`Your code compiled successfully!\``,
-        author: {
-            name: message.author.id,
-            icon_url: message.author.displayAvatarURL()
+
+        let code = args.splice(0).join(" ");
+        if(!code) {
+            return message.channel.send(embedMaker("error", "No Code Supplied", "You didn't supply code for me to execute"));
         }
-    }});
+
+        try {
+            await eval(code);
+        } catch (err) {
+            return message.channel.send(embedMaker("error", "Error", `There was an error while attempting to execute this code: ${err}`));
+        }
+
+        return message.channel.send(embedMaker("success", "Success", `I have successfully executed the code supplied`));
+    }
 }
